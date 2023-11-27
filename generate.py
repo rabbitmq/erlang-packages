1f9f655625bdd4a584a0c758d583927da8a850ca
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2023-11-27 10:17:45 +0100

    Generate Elixir workflows

--- generate.py
+++ generate.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
+import sys
 import os
 import json
 
-configuration_filename = 'erlang-configuration.json'
+project = sys.argv[1]
+package_type = sys.argv[2]
+
+configuration_filename = f"{project}-{package_type}-configuration.json"
 
 with open(configuration_filename, 'r') as f:
   configuration = json.load(f)
@@ -11,32 +15,34 @@ with open(configuration_filename, 'r') as f:
 distributions = configuration["distributions"]
 packages = configuration["packages"]
 
-
 generated_directory = "generated"
 if os.path.exists(generated_directory):
     os.system("rm -rf " + generated_directory)
 
 os.mkdir(generated_directory)
 
-reusable_workflow_template_file = open("erlang-debian-package.template.yml", "r")
+reusable_workflow_template_file = open(os.path.join("templates", project, package_type, "reusable-workflow.yml"), "r")
 reusable_workflow_template = reusable_workflow_template_file.read()
 reusable_workflow_template_file.close()
+
 
 ubuntu_distributions = filter(lambda distribution: distribution["name"] == "ubuntu", distributions)
 ubuntu_versions = " ".join(map(lambda distribution: distribution["version"], ubuntu_distributions))
 
 reusable_workflow = reusable_workflow_template.replace("§ubuntu_versions§", ubuntu_versions)
 
-reusable_workflow_file = open(os.path.join(generated_directory, "gen-erlang-debian-package.yml"), "w") 
+reusable_worflow_filename = f"gen-{package_type}-{project}-reusable-workflow.yml"
+reusable_workflow_file = open(os.path.join(generated_directory, reusable_worflow_filename), "w") 
 reusable_workflow_file.write(reusable_workflow)
 reusable_workflow_file.close() 
 
-workflow_template_file = open("erlang-distribution-debian.template.yml", "r")
+workflow_template_file = open(os.path.join("templates", project, package_type, "workflow.yml"), "r")
 workflow_template = workflow_template_file.read()
 workflow_template_file.close()
 
 for package in packages:
     major_version = package["major"]
+    major_label = major_version.replace(".", "_")
     supported_distributions = package["distributions"]
     for distribution in distributions:
         distribution_name = distribution["name"]
@@ -46,17 +52,20 @@ for package in packages:
         workflow = workflow_template
         if distribution_codename in supported_distributions:
             workflow = workflow\
-                .replace("§erlang_major§", major_version)\
+                .replace("§project§", project)\
+                .replace("§major§", major_version)\
+                .replace("§major_label§", major_label)\
                 .replace("§distribution_label§", distribution_label)\
                 .replace("§distribution_name§", distribution_name)\
                 .replace("§distribution_codename§", distribution_codename)\
                 .replace("§distribution_version§", distribution_version)
-            workflow_file = open(os.path.join(generated_directory, "gen-erlang-" + major_version + "-" + distribution_name + "-" + distribution_version + ".yml"), "w") 
+            workflow_filename = f"gen-{package_type}-{project}-{major_version}-{distribution_name}-{distribution_version}.yml"
+            workflow_file = open(os.path.join(generated_directory, workflow_filename), "w") 
             workflow_file.write(workflow)
             workflow_file.close() 
          
 workflow_directory = ".github/workflows"
-os.system("rm -f " + workflow_directory + "/gen-*.yml")
+os.system(f"rm -f {workflow_directory}/gen-{package_type}-{project}-*.yml")
 os.system("cp " + generated_directory + "/* " + workflow_directory)
 os.system("rm -rf " + generated_directory)
 

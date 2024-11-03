3fa44379456d21fdfad6a70ce398d2573379fe37
Michael Klishin <michael@clojurewerkz.org>
2024-11-03 17:47:50 -0500

    Elixir packages: provision Erlang from Cloudsmith mirrors

--- ci/elixir/deb/build-package.sh
+++ ci/elixir/deb/build-package.sh
@@ -37,10 +37,38 @@ apt-get install -y --no-install-recommends \
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E495BB49CC4BBE5B
 
-keyring_location=/usr/share/keyrings/rabbitmq-rabbitmq-erlang-archive-keyring.gpg
+distro=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+release_codename=$(lsb_release -cs)
 
-curl -1sLf "https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-erlang/gpg.E495BB49CC4BBE5B.key" |  gpg --dearmor > ${keyring_location}
-curl -1sLf "https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-erlang/config.deb.txt?distro=$(lsb_release -is | tr '[:upper:]' '[:lower:]')&codename=$(lsb_release -cs)" > /etc/apt/sources.list.d/rabbitmq-rabbitmq-erlang.list
+apt-get install curl gnupg apt-transport-https -y
+
+## Team RabbitMQ's main signing key
+curl -1sLf "https://keys.openpgp.org/vks/v1/by-fingerprint/0A9AF2115F4687BD29803A206B73A36E6026DFCA" | sudo gpg --dearmor | sudo tee $keyring_location > /dev/null
+## Community mirror of Cloudsmith: modern Erlang repository
+curl -1sLf https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-erlang.E495BB49CC4BBE5B.key | sudo gpg --dearmor | sudo tee /usr/share/keyrings/rabbitmq.E495BB49CC4BBE5B.gpg > /dev/null
+## Community mirror of Cloudsmith: RabbitMQ repository
+curl -1sLf https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-server.9F4587F226208342.key | sudo gpg --dearmor | sudo tee /usr/share/keyrings/rabbitmq.9F4587F226208342.gpg > /dev/null
+
+## Add apt repositories maintained by Team RabbitMQ
+sudo tee /etc/apt/sources.list.d/rabbitmq.list <<EOF
+## Provides modern Erlang/OTP releases
+##
+deb [arch=amd64 signed-by=/usr/share/keyrings/rabbitmq.E495BB49CC4BBE5B.gpg] https://ppa1.rabbitmq.com/rabbitmq/rabbitmq-erlang/deb/$distro $release_codename main
+deb-src [signed-by=/usr/share/keyrings/rabbitmq.E495BB49CC4BBE5B.gpg] https://ppa1.rabbitmq.com/rabbitmq/rabbitmq-erlang/deb/$distro $release_codename main
+
+# another mirror for redundancy
+deb [arch=amd64 signed-by=/usr/share/keyrings/rabbitmq.E495BB49CC4BBE5B.gpg] https://ppa2.rabbitmq.com/rabbitmq/rabbitmq-erlang/deb/$distro $release_codename main
+deb-src [signed-by=/usr/share/keyrings/rabbitmq.E495BB49CC4BBE5B.gpg] https://ppa2.rabbitmq.com/rabbitmq/rabbitmq-erlang/deb/$distro $release_codename main
+
+## Provides RabbitMQ
+##
+deb [arch=amd64 signed-by=/usr/share/keyrings/rabbitmq.9F4587F226208342.gpg] https://ppa1.rabbitmq.com/rabbitmq/rabbitmq-server/deb/$distro $release_codename main
+deb-src [signed-by=/usr/share/keyrings/rabbitmq.9F4587F226208342.gpg] https://ppa1.rabbitmq.com/rabbitmq/rabbitmq-server/deb/$distro $release_codename main
+
+# another mirror for redundancy
+deb [arch=amd64 signed-by=/usr/share/keyrings/rabbitmq.9F4587F226208342.gpg] https://ppa2.rabbitmq.com/rabbitmq/rabbitmq-server/deb/$distro $release_codename main
+deb-src [signed-by=/usr/share/keyrings/rabbitmq.9F4587F226208342.gpg] https://ppa2.rabbitmq.com/rabbitmq/rabbitmq-server/deb/$distro $release_codename main
+EOF
 
 cat > /etc/apt/preferences.d/erlang <<EOF
 Package: erlang*
@@ -48,7 +76,17 @@ Pin: version $ERLANG_VERSION
 Pin-Priority: 1001
 EOF
 
-apt-get update
+## Update package indices
+apt-get update -y
+
+## Install Erlang packages
+apt-get install -y erlang-base \
+                  erlang-asn1 erlang-crypto erlang-eldap erlang-ftp erlang-inets \
+                  erlang-mnesia erlang-os-mon erlang-parsetools erlang-public-key \
+                  erlang-runtime-tools erlang-snmp erlang-ssl \
+                  erlang-syntax-tools erlang-tftp erlang-tools erlang-xmerl
+
+sudo apt-get install rabbitmq-server -y --fix-missing
 
 # --------------------------------------------------------------------
 # Check PGP signing key.
